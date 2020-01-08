58730bac095bfb28b486eb2c18aabb25c9d4a59d
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-01-07 22:46:55 -0500

    city search pulls from API

--- script.js
+++ script.js
@@ -1,93 +1,63 @@
-$(document).ready(function() {
+
+// $(document).ready(function () {
+//     $("#get-weather").click(function () {
+
+//     });
 
 
+    // Create multiple functions within your application to handle the different parts of the dashboard:
+    // Current conditions
+    function currentConditions(city) {
+        $("#currentweather-view").empty();
+        var APIKey = "080e4bdb531f4a9f422244e5bb6b3460";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            // Constructing HTML containing the weather information
+            var cityName = $("<h2>").text(response.name);
+            var iconImage = $("<h2>").text(response.weather[0].icon);
+            var humidityCity = $("<h2>").text(response.main.humidity);
+            var windSpeedCity = $("<h2>").text(response.wind);
+            var UVIndexCity = $("<h2>").text(response.UV);
 
-// Create multiple functions within your application to handle the different parts of the dashboard:
-var city = $("")
-// Current conditions
-function currentCondition(city) {
-    // var APIKey = "080e4bdb531f4a9f422244e5bb6b3460";
-    // var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + city + "id=524901&APPID= " + APIKey;
-    var city = $(this).attr("#get-weather");
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-        city;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+        // Converts the temp to Kelvin with the below formula
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            tempF = tempF.toString().replace(/^(\d+)?([.]?\d{0,2})?$/, "");
+            $("#currentweather-view").append(cityName, tempF);
+        });
 
-        // Constructing HTML containing the weather information
-        // var cityName = $("<h1>").text(response.city);
-        // var weatherDate = $("<a>").text(response.date);
-        // var iconImage = $("<h2>").text(response.icon);
-        // var humidityCity = $("<h2>").text(response.himidity);
-        // var windSpeedCity = $("<h2>").text(response.wind);
-        // var UVIndexCity = $("<h2>").text(response.UV);
-
-        // $("#searches-view").append(cityName, weatherDate, iconImage, humidityCity, windSpeedCity, UVIndexCity);
+    };
+    $("#get-weather").on("click", function (event) {
+        // Preventing the button from trying to submit the form
+        event.preventDefault();
+        // Storing the artist name
+        var inputCity = $(".form-control").val().trim();
+        console.log(inputCity);
+        var button = $("<button>");
+        button.addClass("searchHistory");
+        button.text(inputCity);
+        $("#searches-view").prepend(button);
+        currentConditions(inputCity);
     });
 
-};
-// Event handler for user clicking the select-artist button
-$("#search-weather").on("click", function (event) {
-    // Preventing the button from trying to submit the form
-    event.preventDefault();
-    // Storing the artist name
-    var inputCity = $("form-control").val().trim();
+    // 5-Day Forecast
+    function fiveDayForecast() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // We store all of the retrieved data inside of an object called "response"
+            .then(function (response) {
 
-    // Running the searchBandsInTown function(passing in the artist as an argument)
-    currentConditions(inputCity);
-});
+                // Log the queryURL
+                console.log(queryURL);
 
-// 5-Day Forecast
-function fiveDayForecast() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
+                // Log the resulting object
+                console.log(response);
+            });
 
-            // Log the queryURL
-            console.log(queryURL);
-
-            // Log the resulting object
-            console.log(response);
-        });
-
-};
-// Search history
-function searchHistory() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
-
-            // Log the queryURL
-            console.log(queryURL);
-
-            // Log the resulting object
-            console.log(response);
-        });
-
-};
-// UV index
-function UVIndex() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
-
-            // Log the queryURL
-            console.log(queryURL);
-
-            // Log the resulting object
-            console.log(response);
-        });
-
-};
-};
+    };
